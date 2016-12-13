9c73f00a44818dba233f905aa9ca05e40400dce3
Paulo.Junior <s2it_ppoli@uolinc.com>
2016-12-13 14:46:03 -0200

    assigment3;

---
+++ assignment3/js/app.js
@@ -0,0 +1,74 @@
+(function () {
+'use strict';
+
+angular.module('NarrowItDownApp', [])
+.controller('NarrowItDownController', NarrowItDownController)
+.service('MenuSearchService', MenuSearchService)
+.constant('ApiBasePath', "http://davids-restaurant.herokuapp.com")
+.directive('foundItems', foundItemsDirective);
+
+function foundItemsDirective(){
+	var ddo = {
+		restrict: "E",
+		templateUrl : 'loader/itemsloaderindicator.template.html',
+		scope : {
+			onRemove : '&',
+			items : '<'
+		}
+	};
+	
+	return ddo;
+}
+
+NarrowItDownController.$inject = ['MenuSearchService'];
+function NarrowItDownController(MenuSearchService) {
+	var menu = this;
+	
+	menu.getMatchedMenuItems = function(){
+		var promise = MenuSearchService.getMenu(menu.search);
+		menu.found = [];
+		var notFound = false;
+		
+		promise.then(function (response) {
+			for(var i=0;i<response.data.menu_items.length;i++){
+				if(response.data.menu_items[i].description.indexOf(menu.search)>0){
+					menu.found.push(response.data.menu_items[i]);
+				}
+			}
+			if(menu.found.length===0){
+				menu.notFound = true;
+			}else{
+				menu.notFound = false;
+			}
+		})
+		.catch(function (error) {
+			console.log("Error on load search");
+		});
+	};
+	
+	menu.removeItem = function (itemIndex) {
+		menu.found.splice(itemIndex,1);
+		if(menu.found.length===0){
+			menu.notFound = true;
+		}else{
+			menu.notFound = false;
+		}
+	};
+}
+
+
+MenuSearchService.$inject = ['$http', 'ApiBasePath'];
+function MenuSearchService($http, ApiBasePath) {
+  var service = this;
+
+  service.getMenu = function (description) {
+    var response = $http({
+      method: "GET",
+      url: (ApiBasePath + "/menu_items.json"),
+    });
+	
+    return response;
+  };
+}
+
+})();
