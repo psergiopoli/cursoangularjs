12b542b70babe5c68511ca7a3b1baf07af7ae1f3
Paulo.Junior <s2it_ppoli@uolinc.com>
2017-01-11 15:06:55 -0200

    change domain

--- assignment5/src/common/common.module.js
+++ assignment5/src/common/common.module.js
@@ -2,7 +2,7 @@
 "use strict";
 
 angular.module('common', [])
-.constant('ApiPath', 'https://weffendi-course5.herokuapp.com')
+.constant('ApiPath', 'https://red-cour5.herokuapp.com')
 .config(config);
 
 config.$inject = ['$httpProvider'];
